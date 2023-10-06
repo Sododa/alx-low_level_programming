9438ad742516fec7cbe7e961e581c430104e685e
Sophie Ododa <sophieododa@gmail.com>
2023-10-06 18:36:54 +0000

    six

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,13 +2,13 @@
 
 /**
  *
- * Return: Always 0 (success)
+ * This program prints the digits from 0 to 9 followed by a newline. 
  */
 
 int main(void)
 {
 int num;
-for (num =0; num < 10; num++)
+for (num = 0; num < 10; num++)
 putchar((num % 10) + '0');
 putchar('\n');
 return (0);
