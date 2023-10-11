928bd68b5f047740f34482e086a436b6d7662143
Sophie Ododa <sophieododa@gmail.com>
2023-10-11 18:16:09 +0000

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,7 +4,7 @@
 
 /**
  * print_to_98 - Prints all natural numbers from n to 98
- * @n: The number to start printing from 
+ * @n:The number to start printing from 
  * Return: Always 0.
  */
 void print_to_98(int n)
@@ -12,7 +12,7 @@ void print_to_98(int n)
 {
 if (n <= 98)
 {
-for (; n<= 98; n++)
+for (; n <= 98; n++)
 {
 if (n == 98)
 {
@@ -43,5 +43,3 @@ printf("%d, ", n);
 }
 }
 }
-	
-
