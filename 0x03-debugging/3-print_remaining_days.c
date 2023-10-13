58dd92234ad9ccab7cf4f324f1ac38d2c92e27f2
Sophie Ododa <sophieododa@gmail.com>
2023-10-13 18:37:30 +0000

    Leap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,7 +9,6 @@
  * @year: year
  * Return: void
  */
-
 void print_remaining_days(int month, int day, int year)
 {
 if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
@@ -25,7 +24,7 @@ else
 {
 if (month == 2 && day == 60)
 {
-printf("Invalid date: %02d/%02d/%04d\n", month, day -31, year);
+printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 }
 else
 {
@@ -34,3 +33,4 @@ printf("Remaining days: %d\n", 365 - day);
 }
 }
 }
+
