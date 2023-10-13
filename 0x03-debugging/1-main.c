12bf30f81276ab94a7e8302e73010fe3c70dcf11
Sophie Ododa <sophieododa@gmail.com>
2023-10-13 08:20:29 +0000

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,6 +4,7 @@
  * main - cause infinite loop
  * Return: 0
  */
+
 int main(void)
 {
 int i;
@@ -13,10 +14,11 @@ printf("Infinite loop incoming :(\n");
 i = 0;
 /*
 * while (i < 10)
-* { 
-* putchar(i);
-* } 
+*{ 
+*	putchar(i);
+*}
 */
+
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
