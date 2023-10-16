6f8976780d9902a4350c25fba08fb8bf585caf30
Sophie Ododa <sophieododa@gmail.com>
2023-10-16 21:44:51 +0000

    Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,30 +2,48 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define PASSWORD_LENGTH 63  // The length of the password
-
 /**
  * main - initiating a password
+ * program: crack me
  *result: always 0
  */
 int main(void)
+
 {
-    char password[PASSWORD_LENGTH + 1];
-    int i;
+char password[84];
+int index = 0, sum = 0, diff_half1,diff_half2;
 
-    srand(time(NULL));
+srand(time(0));
 
-    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-
-    for (i = 0; i < PASSWORD_LENGTH; i++)
-    {
-        int index = rand() % (sizeof(charset) - 1);
-        password[i] = charset[index];
-    }
-
-    password[PASSWORD_LENGTH] = '\0';
-
-    printf("%s\n", password);
-
-    return 0;
+while (sum < 2772)
+{
+password[index] = 33 + rand() % 94;
+sum += password[index++];
+}
+password[index] = '\0';
+if (sum != 2772)
+{
+diff_half1 = (sum -2772) / 2;
+diff_half2 = (sum - 2772) / 2;
+if ((sum - 2772) % 2 != 0)
+diff_half1++;
+for (index = 0; password[index]; index++)
+{
+if (password[index] >= (33 + diff_half1))
+{
+password[index] -= diff_half1;
+break;
+}
+}
+for (index = 0; password[index]; index++)
+{
+if (password[index] >= (33 + diff_half2))
+{
+password[index] -= diff_half2;
+break;
+}
+}
+}
+printf("%s", password);
+return (0);
 }
