3e853a8c9b64d27078afcf7f3ecbddcc6045f5e2
Sophie Ododa <sophieododa@gmail.com>
2023-10-16 18:50:58 +0000

    strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,12 +3,14 @@
 /**
  * *_strcpy - copies the string pointed to by src
  * @dest: Character type string
- * @scr: Character type string
+ * @src: Character type string
  * Descrition: copy the string pointed to the pointer 'src' to the buffer
  * pointed to by 'dest'
  * Return: pointer to 'dest'
  */
+
 char *_strcpy(char *dest, char *src)
+
 {
 int z = -1;
 do {
