12781c816d7f05397f2203e178df0a75109f271e
Sophie Ododa <sophieododa@gmail.com>
2023-10-18 16:32:40 +0000

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,8 +13,8 @@ char *_strncat(char *dest, char *src, int n)
 
 int length_of_string, z;
 
-/* z is a counter for n bytes of src to be concatenated*/
-/* length_of_string - length of destination string*/
+/*z is a counter for n bytes of src to be concatenated*/
+/*length_of_string - length of destination string*/
 
 length_of_string = 0;
 while (dest[length_of_string] != '\0')
