fa7aa5b69d8238bce0a6c6dd8140cf15da93da2d
Sophie Ododa <sophieododa@gmail.com>
2023-10-19 08:00:56 +0000

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,21 +1,18 @@
 #include "main.h"
 
 /**
- *_strncat - function that concatenates 
- * @dest: destination string pointer
- * @src: source string pointer
- * @n: number of bytes to be concatenated
- * Result:pointer to destination string
- */
+*_strncat - function that concatenates 
+* @dest: destination string pointer
+* @src: source string pointer
+* @n: number of bytes to be concatenated
+* Result:dest
+*/
 
 char *_strncat(char *dest, char *src, int n)
 {
-
 int length_of_string, z;
-
 /*z is a counter for n bytes of src to be concatenated*/
 /*length_of_string - length of destination string*/
-
 length_of_string = 0;
 while (dest[length_of_string] != '\0')
 {
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- *_strncpy -function that copies a string
- *@dest:destination string pointer
- *@src:source string pointer
- *@n:number of bytes to be used
- *Return:a pointer to string
- */
+*_strncpy -function that copies a string
+*@dest:destination string pointer
+*@src:source string pointer
+*@n:number of bytes to be used
+*Return:a pointer to string
+*/
 
 char *_strncpy(char *dest, char *src, int n)
 {
 int count_of_bytes;
-for (count_of_bytes = 0; count_of_bytes < n && src[count_of_bytes] != '\0'; count_of_bytes++)
+for (count_of_bytes = 0; count_of_bytes < n && src[count_of_bytes] != '\0';count_of_bytes++)
 dest[count_of_bytes] = src[count_of_bytes];
-for (; count_of_bytes < n; count_of_bytes++)
+for (;count_of_bytes < n; count_of_bytes++)
 dest[count_of_bytes] = '\0';
 return (dest);
 }
