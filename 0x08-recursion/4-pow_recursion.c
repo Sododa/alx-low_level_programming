69ce0dd57d13e93ba15ebfea5c844627e0334206
Ubuntu <ubuntu@ip-172-31-16-19.eu-north-1.compute.internal>
2023-10-25 10:31:02 +0000

    Once an idea has taken hold of the brain it's almost impossible to eradicate

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion - return the value of x raised to power of y
+ * @x: raise to the power of y
+ * @y: to be powered
+ * Result:of the power
+ */
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+if (y == 0)
+return (1);
+return (x * _pow_recursion(x, y - 1));
+}
