1197c9889def677e02bac07c040945676fb5e27f
Ubuntu <ubuntu@ip-172-31-16-19.eu-north-1.compute.internal>
2023-10-27 17:35:08 +0000

    Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - multiplies
- * @argc: argument count
- * argv: arguments
- * Return: 0 always
- */
+* main - multiplies two numbers.
+* @argc: argument count
+* @argv: arguments
+*
+* Return: 0
+*/
 int main(int argc, char **argv)
 {
 int x, y;
 if (argc < 3)
 {
-printf ("Error\n");
+printf("Error\n");
 return (1);
 }
-x = atoi (argv[1]);
-y = atoi (argv[2]);
-printf ("%d\n", x * y);
+x = atoi(argv[1]);
+y = atoi(argv[2]);
+printf("%d\n", x * y);
 return (0);
 }
