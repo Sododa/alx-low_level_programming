088bf563354f91f609db138af96f55683988c001
Ubuntu <ubuntu@ip-172-31-16-19.eu-north-1.compute.internal>
2023-11-16 17:47:06 +0000

    The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
  * second_main - check code
+ *
  * Return: always 0
  */
-int second_main(void)
+void second_main(void) __attribute__((constructor));
+void second_main(void)
 {
 printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n");
-return (0);
 }
