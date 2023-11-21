be3d99ad5edc3c588b9d83598531fd1432679b83
Ubuntu <ubuntu@ip-172-31-16-19.eu-north-1.compute.internal>
2023-11-21 14:10:04 +0000

    Print (safe version)

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+/**
+ * print_listint_safe - prints a listint_t linked list.
+ * @head: First node of linked list
+ *This function can print lists with a loop
+ * Return: the number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+size_t number = 0;
+long int diff;
+while (head)
+{
+diff = head - head->next;
+number++;
+printf("[%p] %d\n", (void *)head, head->n);
+if (diff > 0)
+head = head->next;
+else
+{
+printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+break;
+}
+}
+return (number);
+}
