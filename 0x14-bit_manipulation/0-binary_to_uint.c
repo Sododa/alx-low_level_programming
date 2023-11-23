0b1ad2040f34770b000458442e9df9b191b04440
Ubuntu <ubuntu@ip-172-31-16-19.eu-north-1.compute.internal>
2023-11-23 11:02:05 +0000

    0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * binary_to_uin - converts a binary number to an unsigned int.
+ * @b: is pointing to a string of 0 and 1 chars
+ * Return: the converted number, or 0 if
+ */
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int base = 1, number = 0, length = 0;
+if (b == NULL)
+return (0);
+while (b[length])
+length++;
+while (length)
+{
+if (b[length - 1] != '0' && b[length - 1] != '1')
+return (0);
+if (b[length - 1] == '1')
+number += base;
+base *= 2;
+length--;
+}
+return (number);
+}
