c00ef9968963a3fd212a13b85136fc9884dd04ed
Ubuntu <ubuntu@ip-172-31-16-19.eu-north-1.compute.internal>
2023-11-23 11:46:30 +0000

    11

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * set_bit - sets the value of a bit to 1 at a given index.
+ * @index: is the index, starting from 0
+ * of the bit you want to set
+ * @n:unsigned long integer
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+unsigned long int set_bit;
+if (index > (sizeof(unsigned long int) * 8 - 1))
+return (-1);
+set_bit = 1 << index;
+*n = *n | set_bit;
+return (1);
+}
