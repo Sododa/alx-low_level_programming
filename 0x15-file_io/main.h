02b46fa94b6c0e9247d8b28cfb53d4da1b3564a3
Ubuntu <ubuntu@ip-172-31-16-19.eu-north-1.compute.internal>
2023-11-27 13:41:03 +0000

    cp

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <stdio.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
