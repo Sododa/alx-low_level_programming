6b47c3fb96be4649670cd075d365e569f971518e
Ubuntu <ubuntu@ip-172-31-16-19.eu-north-1.compute.internal>
2024-02-01 09:37:11 +0000

    hash tables

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,9 +1,9 @@
 #include "hash_tables.h"
 /**
- * key_index - where key is the key
+ * key_index- where key is the key
  * @key : key is the key
  * @size :  is the size of the array of the hash table
- * @Return:  the index at which the key/value pair
+ * Return:  the index at which the key/value pair
  * should be stored in the array of the hash table
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
